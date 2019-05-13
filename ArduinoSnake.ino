5acd58500397af4ca5bc94fe8181e83cde0a9ddc
Stor Vojtech <vojtech_stor@kb.cz>
2019-05-13 17:04:44 +0200

    rewrited to objects

--- ArduinoSnake.ino
+++ ArduinoSnake.ino
@@ -5,82 +5,50 @@
 #define LCD_BL 2
 extern uint8_t SmallFont[];
 UTFT mlcd(ITDB18SP, 4, 3, 7, 6, 5); //get lcd controler(model,data,clk,cs,rst,control)
+int displayHeight = mlcd.getDisplayYSize();
+int fontHeight = 10;
 #define buttonA 0
 #define buttonB 47
 #define buttonC 91
 #define buttonD 132
 #define buttonE 546
-//char *menuItems[] = {"Pavouk", "Larva", "spider","Lul","Play","to je sranda", "Lul","123","456","4589","123456","456789","123456","7894576","123456","123456","aohj","sadasddsa","sddsaasd","45sa"};
-char *menuItems[] = {"Hrát", "O nás", "Konec"};
-int displayHeight = mlcd.getDisplayYSize();
-int arraySize = 20;
-int topItem = 0;
-int itemsInDisplay = 15;
-int selectedItem = 0;
-int fontHeight = 10;
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(A0, 0); //ADK pin A0
-  pinMode(LCD_BL, OUTPUT);
-  digitalWrite(LCD_BL, HIGH);
-  srand(1024);
-  mlcd.InitLCD(0);
-  mlcd.clrScr(); //refresh the lcd
 
-  // timer.every(500,timelisten); //AUTO GAME
-  // timer.every(10,keyListener); //ADK listen
 
-}
 
-void loop()
-{
-  paint();
-  keyListener();
-}
-void paint()
-{
 
-  mlcd.setFont(SmallFont);
-  for (int i = topItem; i < arraySize && i < itemsInDisplay+topItem; i++)
+
+class Menu{
+  const char* menuItems[3] = {"Hrát", "O nás", "Konec"};
+  int arraySize = 3;
+  int topItem = 0;
+  int itemsInDisplay = 15;
+  int selectedItem = 0;
+  int fontHeight = 10;
+
+public: void render()
+  {
+    Serial.print("render started");
+     mlcd.setFont(SmallFont);
+  for (int i = this->topItem; i < this->arraySize && i < this->itemsInDisplay+this->topItem; i++)
   {
     
     mlcd.setColor(255, 150, 0);    
-    if (i == selectedItem)
+    if (i == this->selectedItem)
     {
       mlcd.setColor(0, 255, 0);
-      mlcd.print(menuItems[i], 10, fontHeight * (i-topItem));
+      mlcd.print(this->menuItems[i], 10, this->fontHeight * (i-this->topItem));
     }
     else
     {     
       Serial.print("top item");
-      Serial.println(topItem);
+      Serial.println(this->topItem);
       Serial.print("i");
       Serial.println(i);
-      mlcd.print(menuItems[i], 10, fontHeight * (i-topItem));
+      mlcd.print(this->menuItems[i], 10, this->fontHeight * (i-this->topItem));
     }
-  }
-}
-void enter()
-{
-    delay(100);
-}
-void up()
-{
-   
-  if(selectedItem != arraySize)
-  {  Serial.println("up pressed");
-     selectedItem++;
-    if(isDownInMenu())
-    {
-      mlcd.clrScr(); 
-      topItem++;
-    }
-   
-  }
-    delay(100);
-}
-bool isDownInMenu()
+  }  
+  } 
+public:  bool isDownInMenu()
 {
   Serial.print("selected Item \0");
   Serial.println(selectedItem);
@@ -94,46 +62,41 @@ bool isDownInMenu()
     return false;
     }
 }
-void down()
+public: void enter()
 {
-  
-  if (selectedItem != 0)
-  { Serial.println("down pressed");
-      
-    if(selectedItem==topItem)
+    delay(100);
+}
+public: void up()
+{
+   
+  if(this->selectedItem != this->arraySize)
+  {  Serial.println("up pressed");
+     this->selectedItem++;
+    if(this->isDownInMenu())
     {
       mlcd.clrScr(); 
-      topItem--;
-    }  
-    selectedItem--;
+      this->topItem++;
+    }
+   
   }
     delay(100);
 }
-void keyListener()
-{
 
-  int value = analogRead(A0);
-  if (value < 150 && value > 100)
-  {
-    enter();
+public: void down()
+{
+  
+  if (this->selectedItem != 0)
+  { Serial.println("down pressed");
+      
+    if(this->selectedItem==topItem)
+    {
+      mlcd.clrScr(); 
+     this->topItem--;
+    }  
+   this->selectedItem--;
   }
-  else if (value < 20)
-  {
-    down();
-  }
-  else if (value > 70 && value < 100)
-  {
-    up();
-  }
+    delay(100);
 }
-class Menu{
-//const  char* menuItems[] = {"Hrát", "O nás", "Konec"};
-  int displayHeight = mlcd.getDisplayYSize();
-  int arraySize = 20;
-  int topItem = 0;
-  int itemsInDisplay = 15;
-  int selectedItem = 0;
-  int fontHeight = 10;
   
   
   };
@@ -156,3 +119,69 @@ class Snake{
       }
     
     };
+
+
+
+
+
+//char *menuItems[] = {"Pavouk", "Larva", "spider","Lul","Play","to je sranda", "Lul","123","456","4589","123456","456789","123456","7894576","123456","123456","aohj","sadasddsa","sddsaasd","45sa"};
+//char *menuItems[] = {"Hrát", "O nás", "Konec"};
+Menu* currentMenu =  new Menu();
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(A0, 0); //ADK pin A0
+  pinMode(LCD_BL, OUTPUT);
+  digitalWrite(LCD_BL, HIGH);
+  srand(1024);
+  mlcd.InitLCD(0);
+  mlcd.clrScr(); //refresh the lcd
+
+  // timer.every(500,timelisten); //AUTO GAME
+  // timer.every(10,keyListener); //ADK listen
+
+}
+
+void loop()
+{
+  paint();
+  keyListener();
+}
+void paint()
+{
+currentMenu->render();
+}
+
+
+void enter()
+{
+  currentMenu->enter();
+   
+}
+void up()
+{
+  currentMenu->up();
+}
+
+void down()
+{
+  currentMenu->down();
+}
+
+void keyListener()
+{
+
+  int value = analogRead(A0);
+  if (value < 150 && value > 100)
+  {
+    enter();
+  }
+  else if (value < 20)
+  {
+    down();
+  }
+  else if (value > 70 && value < 100)
+  {
+    up();
+  }
+}
